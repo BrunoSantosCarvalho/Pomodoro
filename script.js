b41865703d627b1b6c4c96aae89abc7b77233f39
Bruno Carvalho <bruno.leonardosantosdecarvalho@gmail.com>
2024-01-06 23:36:35 -0300

    incluído o parâmetro active de acordo com o contexto

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const curtoBt = document.querySelector('.app__card-button--curto')
 const longoBt = document.querySelector('.app__card-button--longo')
 const banner = document.querySelector('.app__image')
 const titulo = document.querySelector('.app__title')
+const botoes = document.querySelectorAll('.app__card-button')
 
 
 function alterarContexto (contexto) {
@@ -34,15 +35,20 @@ function alterarContexto (contexto) {
 
 focoBt.addEventListener('click', () => {
     alterarContexto('foco')
+    focoBt.classList.add('active')
 })
 
 curtoBt.addEventListener('click', () => {
     alterarContexto('descanso-curto')
+    curtoBt.classList.add('active')
 })
 
 longoBt.addEventListener('click', () => {
     alterarContexto('descanso-longo')
+    longoBt.classList.add('active')
 })
+
+
 
 
 
